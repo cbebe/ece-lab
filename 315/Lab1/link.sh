0948b286fed32db451f7fba25b8a0823db1100c0
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-02-08 23:29:13 -0700

    document link.sh

--- 315/Lab1/link.sh
+++ 315/Lab1/link.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
+################################################################################
+# link.sh
+# Creates a hard link from one file to the main source file in the SDK
+# Makes it easier to switch lab parts
+# and lets you edit in your favourite editor instead of the SDK
+# Usage:
+#   ./link.sh <FILE>
+#   Then every time you make a change, focus on the editor window in the SDK
+#   You'll get a message that the file changed.
+#   Say 'yes' to replace the editor contents
+################################################################################
 
+# Make sure to configure these variables to point to the right file!
 project_dir="$HOME/Documents/School/kypd_ssd_gpio"
 app_dir="$project_dir/kypd_ssd_gpio.sdk/keypad_ssd"
-hello_world_file="$app_dir/src/freertos_hello_world.c"
+main_source_file="$app_dir/src/freertos_hello_world.c"
 
-# cp --remove-destination $1 $hello_world_file
-ln -f $1 $hello_world_file
+ln -f $1 $main_source_file
